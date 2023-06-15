1f58080fcc99be4c329398a50156a5bfa2dd4511
Jorge-Carlos-Guedert <joca12855@gmail.com>
2023-06-15 16:42:11 -0300

    sequence

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,69 +6,101 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  public numero = "0";
-  public numeroAux = "0";
+  public numero = '0';
+  public numeroAux = '0';
+  public operacao = '';
 
   constructor() {}
 
   ngOnInit() {
-    this.numero = "0";
-    this.numeroAux = "0";
+    this.numero = '0';
+    this.numeroAux = '0';
   }
 
   somar() {
-    alert('+');
+    this.operacao = 'mais';
+    this.numeroAux = this.numero;
   }
 
   multiplicar() {
-    alert('X');
+    this.operacao = 'multiplicar';
+    this.numeroAux = this.numero;
   }
 
   dividir() {
-    alert('/');
+    this.operacao = 'dividir';
+    this.numeroAux = this.numero;
   }
 
   subtrair() {
-    alert('-');
+    this.operacao = 'menos';
+    this.numeroAux = this.numero;
   }
 
   clear() {
-    this.numero = "0";
-    this.numeroAux = "0";
+    this.numero = '0';
+    this.numeroAux = '0';
   }
 
   negativo() {
-    if(this.numero.includes('-')){
-      this.numero = (this.numero.replace('-','') + this.numero);
-    }
-    else{
-      this.numero = ("'-' + this.numero")
+    if (this.numero != '0') {
+      this.numero = this.numero.includes('-')
+        ? this.numero.replace('-', '')
+        : '-' + this.numero;
     }
   }
 
   percentual() {
-    alert('%');
+    this.operacao = ''
+    this.numeroAux = ''
+
+    this.numero = (this.converte(this.numero)/ 100).toString().replace('.', ',');
   }
 
   igual() {
-    alert('=');
+    const numeroFinal = this.converte(this.numero);
+    const numeroFinalAux = this.converte(this.numeroAux);
+    
+    switch (this.operacao){
+      case 'somar' :
+        this.numero = this.numero + this.numeroAux ;
+        break;
+
+      case 'subtrair' :
+          
+        break;  
+
+      case 'multlipicar' :
+          
+        break; 
+
+      case 'dividir' :
+          
+        break;  
+    }
   }
 
   numeros(num: string) {
-    if(this.numero.startsWith('0')){
-      this.numero =""
+    if (this.numero.startsWith('0') && this.numero.startsWith('0,')) {
+      this.numero = '';
     }
 
-    if(this.numero.length <= 8){
+    if (this.numero.length <= 8) {
       this.numero += num;
-
     }
   }
 
   virgula() {
-    if(this.numero.length <= 8 && !this.numero.includes(',')){
+    if (this.numero.length <= 8 && !this.numero.includes(',')) {
       this.numero += ',';
+    }
+  }
 
+  converte(dados: string): number {
+    if (dados.includes(',')) {
+      return parseFloat(dados.replace(',', '.'));
+    } else {
+      return parseInt(dados);
     }
   }
 }
